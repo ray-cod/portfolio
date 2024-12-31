a286de4cf9386853cf65b482b4439031244ac1d4
rlassissi023 <rlassissi023@student.wethinkcode.co.za>
2024-12-31 09:09:20 +0200

    fixed bugs with the contact form

--- js/add-on.js
+++ js/add-on.js
@@ -1,36 +1,34 @@
-document.addEventListener("DOMContentLoaded", function () {
-    emailjs.init("yWZJLoIUu98bXnUwK"); // Initialize EmailJS with your public key
-});
-
 function sendMail(event) {
     event.preventDefault(); // Prevent form submission
 
     // Collect form data
-    let params = {
+    let templateParams = {
         name: document.getElementById("name").value,
         email: document.getElementById("email").value,
         subject: document.getElementById("subject").value,
         message: document.getElementById("message").value,
     };
 
+    console.log("sendMail function called");
+    console.log(templateParams);
+
+
     // Send email via EmailJS
-    emailjs
-        .send("service_4oyap97", "template_cy0sa6q", params)
-        .then(
-            function (response) {
-                alert("Email sent successfully!");
-                console.log("SUCCESS!", response);
-            },
-            function (error) {
-                alert("Failed to send email. Please try again.");
-                console.error("ERROR:", error);
-            }
-        );
+    emailjs.send('service_aoeie78', 'template_cy0sa6q', templateParams).then(
+        (response) => {
+            alert("Your message has been sent")
+            console.log('SUCCESS!', response.status, response.text);
+        },
+        (error) => {
+            alert("An error occurred!")
+            console.log('FAILED...', error);
+        },
+    );
 }
 
 function toggleContent() {
-    const moreText = document.getElementById("more-text");
-    const moreButton = document.getElementById("more-button");
+    const moreText = document.getElementsByClassName("more-text");
+    const moreButton = document.getElementsByClassName("more-button");
 
     if (moreText.style.display === "none") {
         moreText.style.display = "inline";
